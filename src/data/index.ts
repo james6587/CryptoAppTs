5973018491ec2037dc25f09a81cb1d3ae702916d
James <jameseliotv@gmail.com>
2024-08-19 14:30:14 -0500

    Crypto app

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+import { Currency } from "../types";
+
+export const currencies : Currency[] = [
+  { code: 'USD', name: 'U.S. dollar'},
+  { code: 'MXN', name: 'Mexican Peso'},
+  { code: 'EUR', name: 'Euro'},
+  { code: 'GBP', name: 'British Pound Sterling'},
+  { code: 'COP', name: 'Colombian Peso'},
+]
